0976a903b842df2228f39884645ba00fa383ae05
lida-skakun <kunska@gmail.com>
2022-11-06 14:36:26 +0100

    styles finished created new component Description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Description from "./Description";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App container-fluid">
+      <div className="input-group mb-3">
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Type a city..."
+          aria-describedby="basic-addon2"
+        />
+        <span className="input-group-text search-button" id="basic-addon2">
+          🔎
+        </span>
+      </div>
       <div className="row">
-        <div className="col-6">
+        <div className="col-5">
           <h3>
             <strong>New York</strong>, USA
           </h3>
@@ -18,24 +31,15 @@ function App() {
                 className="weather-icon"
               />
             </li>
-            <li className="temperature">
-              25° <p className="description">Stormy</p>
-            </li>
-            <li className="desription">Stormy</li>
-            <li>
-              <span>Stormy</span>
-              <p>Wind</p>
-            </li>
-            <li>
-              <p>Wind</p>
-              <p>Wind</p>
-            </li>
+            <li className="temperature">25°</li>
+            <li className="parameters">Stormy</li>
           </ul>
         </div>
-        <div className="col-6">
-          <form>
-            <input type="serach" placeholder="Type a city..." />
-          </form>
+        <div className="col-7 additional-conditions">
+          <Description parametr="Wind" icon="⩬" value="12,4 km/h" />
+          <Description parametr="Index UV" icon="⨀" value={0} />
+          <Description parametr="Temperature" icon="⫰" value="25°" />
+          <Description parametr="Humidity" icon="⩫" value="75%" />
         </div>
       </div>
     </div>
---
+++ src/Description.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Description.css";
+
+export default function Description(props) {
+  return (
+    <div className="Description row p-1 m-1 shadow rounded details">
+      <div className="col-3 mt-2">{props.icon}</div>
+      <div className="col-7 text-start p-0">
+        <p className="parameters">{props.parametr}</p>
+        <p>
+          <strong>{props.value}</strong>
+        </p>
+      </div>
+    </div>
+  );
+}
